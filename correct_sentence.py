dd3fe973155950d22c04ce98a62e38f7f537e011
Namrata Simha <npsimha@ucsc.edu>
2017-12-14 19:13:52 -0800

    Added Rule Generation for tests and better pre-processing

--- correct_sentence.py
+++ correct_sentence.py
@@ -4,6 +4,10 @@ from collections import defaultdict, Counter
 import itertools
 import pickle
 import re
+import random
+import datetime
+import sys
+
 
 def probability(s, model):
     final_prob = 1
@@ -32,25 +36,63 @@ def probability(s, model):
 
     return final_prob
 
-def test_sentence_combinations(sentence, model):
+def test_sentence_combinations(sentence, model, presets):
     s = []
     for word in sentence:
         s.append(pos_tag(word_tokenize(word))[0])
-    perms = list(itertools.permutations(s))
+    #print(s)
+    
+    compare = []
+    for x in s:
+        compare.append(x[1])
+    #print(compare)
+    
+    list_presets = []
+    for p in presets:
+        if Counter(compare) == Counter(p):
+            list_presets.append(p)
+    #print(list_presets)
+    
     best = s
     p = 0
-    for sent in perms:
-        new_p = probability(sent, model)
-        if new_p > p:
-            p = new_p
-            best = sent
+    
+    
+    if len(list_presets)>0:
+        temp_sent = list(s)
+        for pre in list_presets:
+            new_sent = []
+            for pos in pre:
+                for word in temp_sent:
+                    if word[1] == pos:
+                        new_sent.append(word)
+                        break
+                lis = []
+                for word in temp_sent:
+                    if not word[1] == pos:
+                        lis.append(word)
+                temp_sent = list(lis)
+            new_p = probability(new_sent, model)
+            if new_p > p:
+                p = new_p
+                best = new_sent
+                #print(best)
+    else:
+        perms = list(itertools.permutations(s))
+        for sent in perms:
+            #print(sent)
+            new_p = probability(sent, model)
+            if new_p > p:
+                p = new_p
+                best = sent
 
     #final sentence formatting
     final_sentence = ""
     count = 0
     flag = 1
-    if best[len(best)-1][0] == '.':
-        flag = 0
+    
+    if len(best)>=1:
+        if best[len(best)-1][0] == '.':
+            flag = 0
     for w in best:
         final_sentence += w[0]
         if flag == 1:
@@ -60,21 +102,84 @@ def test_sentence_combinations(sentence, model):
         count += 1
 
     return final_sentence
+
+def jumble_sentence(sent):
+    random.shuffle(sent)
+    return sent
+
+
+#######################################################################
+
+print("Start time: "+str(datetime.datetime.now()))
+print()
+
 model = []
 with open("model.txt", 'rb') as f:
     model = pickle.load(f)
-#preliminary test data
-print("Input sentence:")
-s = ['a', 'am', 'I', 'Dancer', '.']
-s = ['doctor', 'are', 'a', 'You', '.']
-s = ['Boy', 'plays', 'park', 'in', 'the']
-s = ['reads', 'Mary', 'a', 'book']
-s = ['car', 'drives', 'the', 'Sam']
-sent = ""
-for w in s:
-    sent = sent+w+" "
-print("\t"+sent)
+#print(model)
+with open("test.txt", 'rb') as f:
+    test = pickle.load(f)
+with open("sentenceModels.txt", 'rb') as f:
+    sModel = pickle.load(f)
+presets = []
+for k in sModel:
+    temp = []
+    key = re.split(" ", k)
+    for w in key:
+        if not w == '':
+            temp.append(w)
+    presets.append(temp)
 
-print("Output sentence:")
-final_sentence = test_sentence_combinations(s, model)
-print("\t"+final_sentence)
+total = len(test)
+count = 0
+
+i = 0
+l = len(test)
+byfive = []
+for a in range(1,20):
+    byfive.append(a*(int(l/20)))
+byfive.append(l)
+#print(byfive)
+print("Correcting Sentences and Checking Accuracy of Corrections:")
+print("--------------------100%")
+for s in test:
+    i += 1
+    #print(i)
+    if i in byfive:
+        sys.stdout.write('#')
+        sys.stdout.flush()
+    sent = list(s)
+    #print(sent)
+    temp = jumble_sentence(s)
+    #print(temp)
+    final_sentence = test_sentence_combinations(temp, model, presets)
+    new_sent = re.split(" ", final_sentence)
+    n = []
+    for x in new_sent:
+        if not x == '':
+            n.append(x)
+    if n == sent:
+        count += 1
+
+accuracy = count*100/total
+print()
+print("Percentage correctly corrected sentences:"+str(accuracy))
+
+#preliminary test data
+'''
+se = [['a', 'am', 'I', 'Dancer'], ['doctor', 'are', 'a', 'You'], ['Boy', 'plays', 'park', 'in', 'the'], ['reads', 'Mary', 'a', 'book'], ['car', 'drives', 'the', 'Sam']]
+for s in se:
+    print("Input sentence:")
+    sent = ""
+    for w in s:
+        sent = sent+w+" "
+    print("\t"+sent)
+
+    print("Output sentence:")
+    final_sentence = test_sentence_combinations(s, model, presets)
+    print("\t"+final_sentence)
+    
+    print("**************************************")
+'''
+print()
+print("End time: "+str(datetime.datetime.now()))
--- train_model.py
+++ train_model.py
@@ -4,10 +4,14 @@ from collections import defaultdict, Counter
 import itertools
 import pickle
 import re
+import time
+import sys
+import datetime
+
 
 def make_dataset():
     s = []
-    final_s = []
+    final_s = [['My', 'name', 'is', 'Namrata'], ['I', 'am', 'an', 'Engineer'], ['I', 'am', 'a', 'Singer'], ['I', 'was', 'a', 'karate', 'player'], ['You', 'are', 'a', 'singer']]
     for i in range(1990, 2013):
         filename = "spoken_eng/w_spok_" + str(i) + ".txt"
         #filename = "spoken_eng/w_spok_1990.txt"
@@ -28,7 +32,13 @@ def make_dataset():
         sentences = re.sub("voice-over [a-zA-Z]+ ,", "", sentences)
         sentences = re.sub(" : ", " ", sentences)
         sentences = re.sub("@", "", sentences)
+        sentences = re.sub(",", "", sentences)
         sentences = re.sub("voice-over", "", sentences)
+        sentences = re.sub("Voice-over", "", sentences)
+        sentences = re.sub("Voice over", "", sentences)
+        sentences = re.sub("Voiceover", "", sentences)
+        sentences = re.sub("--", "", sentences)
+        sentences = re.sub("voiceover", "", sentences)
         sentences = re.sub("voice over", "", sentences)
         sentences = re.sub("Mr[.]", "", sentences)
         sentences = re.sub("Ms[.]", "", sentences)
@@ -37,11 +47,23 @@ def make_dataset():
         sentences = re.sub("U[.]S", "US", sentences)
         sentences = re.sub("U[.]S[.]S[.]R[.]", "USSR", sentences)
         sentences = re.split("[.]", sentences)
+        temp = []
+        for sen in sentences:
+            temp += re.split("[?] ", sen)
+        #print(temp)
+        sentences = temp
         for sen in sentences:
             #print(sen)
-            new = sen+"."
+            #new = sen+"."
+            new = sen
             s.append(re.split(" ", new))
+
         for x in s:
+            #print(x)
+            if not x:
+                continue
+            if x == [""] or x == ["."]:
+                continue
             words = []
             for word in x:
                 if word == "\n":
@@ -59,7 +81,20 @@ def find_pos(sents):
     print("Finding POS Tags")
     new_sents = []
     #print(sents)
+    i = 0
+    l = len(sents)
+    byfive = []
+    for a in range(1,20):
+        byfive.append(a*(int(l/20)))
+    byfive.append(l)
+    #print(byfive)
+    print("--------------------100%")
     for s in sents:
+        i += 1
+        #print(i)
+        if i in byfive:
+            sys.stdout.write('#')
+            sys.stdout.flush()
         new_s = []
         for word in s:
             new_s.append(pos_tag(word_tokenize(word))[0])
@@ -68,17 +103,32 @@ def find_pos(sents):
     return new_sents
 
 def find_probs(sents):
+    print()
+    print()
     print("Finding Probabilities")
+    print("--------------------100%")
     model = defaultdict(lambda: 0)
     for sentence in sents:
         for w1, w2 in bigrams(sentence):
             model[(w1[1], w2[1])] += 1
-
+    
     counts = dict(Counter(model))
     probs = dict(Counter(model))
-
+    sys.stdout.write('#')
+    i = 0
+    l = len(counts)
+    byfive = []
+    for a in range(1,19):
+        byfive.append(a*(int(l/20)))
+    byfive.append(l)
+    #print(byfive)
     for tup in counts:
         n = 0
+        i += 1
+        #print(i)
+        if i in byfive:
+            sys.stdout.write('#')
+            sys.stdout.flush()
         for w in counts:
             if w[0] == tup[0]:
                 n += counts[w]
@@ -86,13 +136,57 @@ def find_probs(sents):
         #print(probs[tup])
 
     return probs
+    
+def storeSentModels(sents):
+    temp = []
+    for s in sents:
+        new_s = ""
+        for w in s:
+            new_s = new_s + w[1] + " "
+        temp.append(new_s)
+    new_sents = dict(Counter(temp))
+    #print(new_sents)
+    # store test data in file
+    with open("sentenceModels.txt", 'wb') as f:
+        pickle.dump(new_sents, f)
 
+
+print("Start time: "+str(datetime.datetime.now()))
+print()
 #preliminary test data
-sents = [['My', 'name', 'is', 'Namrata', '.'], ['I', 'am', 'an', 'Engineer', '.'], ['I', 'am', 'a', 'Singer', '.'], ['I', 'was', 'a', 'karate', 'player', '.']]
-sents = sents+make_dataset()
+#sents = [['My', 'name', 'is', 'Namrata', '.'], ['I', 'am', 'an', 'Engineer', '.'], ['I', 'am', 'a', 'Singer', '.'], ['I', 'was', 'a', 'karate', 'player', '.']]
+#sents = sents+make_dataset()
+sents = make_dataset()
+temporary = []
+for s in sents:
+    if len(s) <= 7:
+        temporary.append(s)
+sents = temporary
+l = len(sents)
+train_length = int(0.6*l)
+test_length = l-train_length
+train = sents[:train_length]
+test = sents[train_length:]
+# store test data in file
+with open("test.txt", 'wb') as f:
+    pickle.dump(test, f)
+
+print("Total:"+str(l))
+print("Train:"+str(train_length))
+print("Test:" + str(test_length))
+
+sents = train
+
 sents = find_pos(sents)
+
+storeSentModels(sents)
+
 model = find_probs(sents)
 
 #store model in file
 with open("model.txt", 'wb') as f:
     pickle.dump(model, f)
+
+print()
+print()
+print("End time: "+str(datetime.datetime.now()))
